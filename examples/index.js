37625ff38ed5cb04f8750dd659accfe0d910d3d9
clementreiffers <reiffersclement@gmail.com>
2022-12-04 01:25:50 +0100

    examples rewrited and danfojs-node added to dependencies in examples

--- examples/index.js
+++ examples/index.js
@@ -1,15 +1,13 @@
 // Here let see examples of how to use the api
 
-// GET HTTP
-const toJson = data => data.json();
+const _toJson = data => data.json();
 
-const get = link => fetch(link)
-	.then(toJson)
+const _get = link => fetch(link)
+	.then(_toJson)
 	.then(console.log)
 	.catch(console.log);
 
-// Example POST method implementation:
-const getFetchPostOptn = data => ({
+const _getFetchPostOption = data => ({
 	method: "POST",
 	headers: {
 		"Content-Type": "application/json",
@@ -17,16 +15,19 @@ const getFetchPostOptn = data => ({
 	body: JSON.stringify(data),
 });
 
-const post = (url = "", data = {}) =>
-	fetch(url, getFetchPostOptn(data))
+const _post = (url = "", data = {}) =>
+	fetch(url, _getFetchPostOption(data))
 		.then(console.log)
 		.catch(console.log);
 
-// To post only one website
-post("http://localhost:5000/add", {url: "je-suis-un-gros-test.pas-un-site-officiel"})
-	.then(() => get("http://127.0.0.1:5000/get-all")); // To see if it has been operated
+// POST HTTP
+const _dataToPost = {url: "je-suis-un-gros-test.pas-un-site-officiel"};
+_post("http://localhost:5000/add", _dataToPost)
+	.then(() => _get("http://127.0.0.1:5000/get-all")); // To see if it has been operated
 
-get("http://127.0.0.1:5000/get-all");
-get("http://127.0.0.1:5000/get?url=youtube.fr");
-get("http://127.0.0.1:5000/is-url-exists?url=youtube.fr");
+// GET HTTP
+
+_get("http://127.0.0.1:5000/get-all");
+_get("http://127.0.0.1:5000/get?url=youtube.fr");
+_get("http://127.0.0.1:5000/is-url-exists?url=youtube.fr");
 
